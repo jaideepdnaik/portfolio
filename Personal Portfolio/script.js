590fec079fcd9b72448a22caa1de84b9b81339dc
Jaideep D Naik <77848433+jaideepdnaik@users.noreply.github.com>
2023-09-10 09:38:10 +0530

    Add files via upload

---
+++ Personal Portfolio/script.js
@@ -0,0 +1,49 @@
+//Toggle icon navbar
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
+
+
+// Scroll Sections 
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 100;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if (top >= offset && top < offset + height) {
+            // active navbar links 
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+            //Active sections for animation on scroll
+        }
+        //If want to use animation that repeats on scroll use this
+        else {
+            sec.classList.add('show-animate');
+        }
+    });
+
+    //Sticky header
+    let header = document.querySelector('header');
+
+    header.classList.toggle('sticky', window.scrollY > 100);
+
+    // Remove toggle icon and navbar when click navbar links(Scroll)
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
+
+    //Animation footer on scroll
+    let footer = document.querySelector('footer');
+    footer.classList.toggle('show-animate', this.innerHeight + this.scrollY >= document.scrollingElement.scrollHeight);
+}
